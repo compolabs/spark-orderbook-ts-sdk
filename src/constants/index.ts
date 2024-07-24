4d80c0cf2436814324422eb2fd892e84c9093505
Anton Zeunov <lalow.main@gmail.com>
2024-07-24 08:14:48 +0300

    feat: update market contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,7 +6,7 @@ export const DEFAULT_GAS_PRICE = "1";
 export const DEFAULT_GAS_LIMIT_MULTIPLIER = "2";
 
 export const BETA_CONTRACT_ADDRESSES: OrderbookContracts = {
-  market: "0xab79d9f442d1e0ea9857aeab97f054290bffbe962cfef6517f9538f7b913170e",
+  market: "0x08ca18ed550d6229f001641d43aac58e00f9eb7e25c9bea6d33716af61e43b2a",
   tokenFactory:
     "0x3141a3f11e3f784364d57860e3a4dcf9b73d42e23fd49038773cefb09c633348",
   pyth: "0x3cd5005f23321c8ae0ccfa98fb07d9a5ff325c483f21d2d9540d6897007600c9",
@@ -20,4 +20,4 @@ export const TESTNET_NETWORK = {
 };
 
 export const TESTNET_INDEXER_URL =
-  "https://indexer.bigdevenergy.link/67b693c/v1/graphql";
+  "https://indexer.bigdevenergy.link/029c37c/v1/graphql";
