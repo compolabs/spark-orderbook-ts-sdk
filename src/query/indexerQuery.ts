b93fe53dc17cf1279da3014d8f8b4e07dd56f4c7
Anton Rock <anton@composabilitylabs.org>
2025-02-04 16:15:00 +0300

    feat: hotfix sort

--- src/query/indexerQuery.ts
+++ src/query/indexerQuery.ts
@@ -7,55 +7,17 @@ export const getOrdersQuery = (
   type: "query" | "subscription",
   params: GetOrdersParams,
 ): QueryOptions => {
-  const { limit, offset, ...restParams } = params;
+  const { limit, orderType, offset, ...restParams } = params;
+  const priceOrder = orderType === "Buy" ? "desc" : "asc";
   const offsetInRange = offset ?? 0;
-
   const query = gql`
     ${type} OrderQuery(
       $limit: Int!
       $offset: Int!
       $where: Order_bool_exp
+      $priceOrder: order_by!
     ) {
-      Order(limit: $limit, offset: $offset, where: $where) {
-        id
-        asset
-        amount
-        initialAmount
-        orderType
-        price
-        status
-        user
-        timestamp
-        market
-      }
-    }
-  `;
-
-  return {
-    query,
-    variables: {
-      limit,
-      offset: offsetInRange,
-      where: generateWhereFilter(restParams),
-    },
-  };
-};
-
-export const getActiveOrdersQuery = (
-  type: "query" | "subscription",
-  params: GetActiveOrdersParams,
-): QueryOptions => {
-  const { limit, orderType, offset, ...restParams } = params;
-  const queryObject = `Active${orderType}Order`;
-  const offsetInRange = offset ?? 0;
-
-  const query = gql`
-    ${type} ${queryObject}Query(
-      $limit: Int!
-      $offset: Int!
-      $where: ${queryObject}_bool_exp
-    ) {
-      ${queryObject}(limit: $limit, offset: $offset, where: $where) {
+      Order(limit: $limit, offset: $offset, where: $where, order_by: { price: $priceOrder }) {
         id
         asset
         amount
@@ -76,6 +38,48 @@ export const getActiveOrdersQuery = (
       limit,
       offset: offsetInRange,
       where: generateWhereFilter({ ...restParams, orderType }),
+      priceOrder,
+    },
+  };
+};
+
+export const getActiveOrdersQuery = (
+  type: "query" | "subscription",
+  params: GetActiveOrdersParams,
+): QueryOptions => {
+  const { limit, orderType, offset, ...restParams } = params;
+  const priceOrder = orderType === "Buy" ? "desc" : "asc";
+  const queryObject = `Active${orderType}Order`;
+  const offsetInRange = offset ?? 0;
+  const query = gql`
+    ${type} ${queryObject}Query(
+      $limit: Int!
+      $offset: Int!
+      $where: ${queryObject}_bool_exp
+      $priceOrder: order_by!
+    ) {
+      ${queryObject}(limit: $limit, offset: $offset, where: $where, order_by: { price: $priceOrder })) {
+        id
+        asset
+        amount
+        initialAmount
+        orderType
+        price
+        status
+        user
+        timestamp
+        market
+      }
+    }
+  `;
+
+  return {
+    query,
+    variables: {
+      limit,
+      offset: offsetInRange,
+      where: generateWhereFilter({ ...restParams, orderType }),
+      priceOrder,
     },
   };
 };
